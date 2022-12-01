2ad8b3fd6aa2f37d906611511fba08b4046a4020
Magdalena <mkceva@gmail.com>
2022-12-01 23:38:39 +0100

    index controller

---
+++ src/main/java/guru/springframework/sfgrecipes/IndexController.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgrecipes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"","/","/index"})
+    public String getIndex(){
+        return "index";
+    }
+}
